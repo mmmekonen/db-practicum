2d42b64cc21146dfe0b9209f3239351d5331cb5c
jeffalipson <135264871+jeffalipson@users.noreply.github.com>
2023-09-12 16:53:37 -0400

    Updated QPB and Projection Operator for * clause

--- src/main/java/common/QueryPlanBuilder.java
+++ src/main/java/common/QueryPlanBuilder.java
@@ -4,28 +4,40 @@ import java.util.ArrayList;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.AllColumns;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
 import net.sf.jsqlparser.statement.select.SelectItem;
 import operator.*;
 
 /**
- * Class to translate a JSQLParser statement into a relational algebra query plan. For now only
- * works for Statements that are Selects, and specifically PlainSelects. Could implement the visitor
- * pattern on the statement, but doesn't for simplicity as we do not handle nesting or other complex
+ * Class to translate a JSQLParser statement into a relational algebra query
+ * plan. For now only
+ * works for Statements that are Selects, and specifically PlainSelects. Could
+ * implement the visitor
+ * pattern on the statement, but doesn't for simplicity as we do not handle
+ * nesting or other complex
  * query features.
  *
- * <p>Query plan fixes join order to the order found in the from clause and uses a left deep tree
- * join. Maximally pushes selections on individual relations and evaluates join conditions as early
- * as possible in the join tree. Projections (if any) are not pushed and evaluated in a single
- * projection operator after the last join. Finally, sorting and duplicate elimination are added if
+ * <p>
+ * Query plan fixes join order to the order found in the from clause and uses a
+ * left deep tree
+ * join. Maximally pushes selections on individual relations and evaluates join
+ * conditions as early
+ * as possible in the join tree. Projections (if any) are not pushed and
+ * evaluated in a single
+ * projection operator after the last join. Finally, sorting and duplicate
+ * elimination are added if
  * needed.
  *
- * <p>For the subset of SQL which is supported as well as assumptions on semantics, see the Project
+ * <p>
+ * For the subset of SQL which is supported as well as assumptions on semantics,
+ * see the Project
  * 2 student instructions, Section 2.1
  */
 public class QueryPlanBuilder {
-  public QueryPlanBuilder() {}
+  public QueryPlanBuilder() {
+  }
 
   /**
    * Top level method to translate statement to query plan
@@ -43,14 +55,22 @@ public class QueryPlanBuilder {
 
     if (where == null && selects.size() == 0) {
       return new ScanOperator(table.getName());
-    }
-    if (where == null && selects.size() > 0) {
-      return new ProjectionOperator(selects, new ScanOperator(table.getName()));
-    }
-    if (where != null && selects.size() == 0) {
+    } else if (where == null && selects.size() > 0) {
+      if (selects.get(0) instanceof AllColumns) {
+        return new ScanOperator(table.getName());
+      } else {
+        return new ProjectionOperator(selects, new ScanOperator(table.getName()));
+      }
+    } else if (where != null && selects.size() == 0) {
       return new SelectOperator(table.getName(), new ScanOperator(table.getName()), where);
+    } else if (where != null && selects.size() > 0) {
+      if (selects.get(0) instanceof AllColumns) {
+        return new SelectOperator(table.getName(), new ScanOperator(table.getName()), where);
+      } else {
+        return new ProjectionOperator(selects,
+            new SelectOperator(table.getName(), new ScanOperator(table.getName()), where));
+      }
     }
-    return new ProjectionOperator(
-        selects, new SelectOperator(table.getName(), new ScanOperator(table.getName()), where));
+    return null;
   }
 }
--- src/main/java/operator/ProjectionOperator.java
+++ src/main/java/operator/ProjectionOperator.java
@@ -15,25 +15,19 @@ public class ProjectionOperator extends Operator {
   public ProjectionOperator(List<SelectItem> selectItems, Operator child) {
     super(child.outputSchema);
     this.child = child;
-    if (selectItems.size() > 0 && selectItems.get(0) instanceof AllColumns) {
-      this.projectionColumnsIndices = new ArrayList<>();
-      for (int i = 0; i < child.outputSchema.size(); i++) {
-        this.projectionColumnsIndices.add(i);
-      }
-    } else {
-      this.projectionColumnsIndices = new ArrayList<>();
-      for (SelectItem selectItem : selectItems) {
-        SelectExpressionItem selectExpressionItem = (SelectExpressionItem) selectItem;
-        Column expression = (Column) selectExpressionItem.getExpression();
-        String columnName = expression.getColumnName();
 
-        int index = 0;
-        for (Column column : child.outputSchema) {
-          if (column.getColumnName().equals(columnName)) {
-            this.projectionColumnsIndices.add(index);
-          }
-          index++;
+    this.projectionColumnsIndices = new ArrayList<>();
+    for (SelectItem selectItem : selectItems) {
+      SelectExpressionItem selectExpressionItem = (SelectExpressionItem) selectItem;
+      Column expression = (Column) selectExpressionItem.getExpression();
+      String columnName = expression.getColumnName();
+
+      int index = 0;
+      for (Column column : child.outputSchema) {
+        if (column.getColumnName().equals(columnName)) {
+          this.projectionColumnsIndices.add(index);
         }
+        index++;
       }
     }
   }
@@ -45,16 +39,11 @@ public class ProjectionOperator extends Operator {
 
   @Override
   public Tuple getNextTuple() {
-    // System.out.println("in get next tuple");
 
     Tuple tuple = child.getNextTuple();
     if (tuple == null) {
       return null;
     }
-
-    // for (int i = 0; i < projectionColumnsIndices.size(); i++) {
-    //   System.out.println(projectionColumnsIndices.get(i));
-    // }
 
     ArrayList<Integer> tupleValues = new ArrayList<>();
     for (int i = 0; i < projectionColumnsIndices.size(); i++) {
