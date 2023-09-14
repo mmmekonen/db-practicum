533316320778c1941b186e6fe62dfa7346d63b78
mmmekonen <maelat.mekonen@gmail.com>
2023-09-14 14:00:16 -0400

    deleted build files to ignore

--- build/resources/test/samples/input/queries2.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM Sailors WHERE Sailors.A >= 4;
-SELECT * FROM Sailors WHERE Sailors.B < Sailors.C;
-SELECT Sailors.A, Sailors.B FROM Sailors WHERE Sailors.A >= 4;
-SELECT Sailors.A, Sailors.B FROM Sailors;
-SELECT Sailors.C, Sailors.B, Sailors.A FROM Sailors WHERE Sailors.A >= 0;
-SELECT * FROM Sailors S WHERE S.A >= 4;
-SELECT S.A, S.B FROM Sailors S WHERE S.A >= 4;
-SELECT S.A, S.B FROM Sailors S;
-SELECT S.C, S.B, S.A FROM Sailors S WHERE S.A >= 0;
