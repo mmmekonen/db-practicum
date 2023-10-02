270c9ec5a464a2faef380aa5f8ded9f01001bbf6
jeffalipson <135264871+jeffalipson@users.noreply.github.com>
2023-10-02 19:57:27 -0400

    TupleReader and TupleWriter implemented

---
+++ src/main/java/common/TupleReader.java
@@ -0,0 +1,78 @@
+package common;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+
+public class TupleReader {
+  private FileInputStream fileInputStream;
+  private FileChannel fileChannel;
+  private ByteBuffer buffer;
+  private int numTuples;
+  private int attributes;
+  private static int PAGE_SIZE = 4096;
+  private int tuplesRemaining;
+
+  public TupleReader(String fileName) throws IOException {
+    this.fileInputStream = new FileInputStream(fileName);
+    this.fileChannel = fileInputStream.getChannel();
+    this.buffer = ByteBuffer.allocate(PAGE_SIZE);
+    readPageHeader();
+
+  }
+
+  private boolean readPageHeader() throws IOException {
+    buffer.clear();
+    if (fileChannel.read(buffer) != -1) {
+      buffer.flip();
+      this.attributes = buffer.getInt();
+      this.numTuples = buffer.getInt();
+      this.tuplesRemaining = numTuples;
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  public int getAttributes() {
+    return attributes;
+  }
+
+  public int getNumTuples() {
+    return numTuples;
+  }
+
+  public Tuple readNextTuple() throws IOException {
+    // Read the next page header if the current page is exhausted
+    if (buffer.remaining() < (attributes * 4)) {
+      boolean more = readPageHeader();
+      if (more == false) {
+        return null;
+      }
+    }
+
+    if (tuplesRemaining == 0) {
+      return null;
+    }
+
+    ArrayList<Integer> tuple = new ArrayList<Integer>();
+    for (int i = 0; i < attributes; i++) {
+      int attributeValue = buffer.getInt();
+      tuple.add(attributeValue);
+    }
+
+    this.tuplesRemaining--;
+
+    Tuple t = new Tuple(tuple);
+    return t;
+  }
+
+  public void close() throws IOException {
+    fileChannel.close();
+    fileInputStream.close();
+  }
+
+}
---
+++ src/main/java/common/TupleWriter.java
@@ -0,0 +1,76 @@
+package common;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.io.File;
+
+public class TupleWriter {
+  private static int PAGE_SIZE = 4096;
+  private FileOutputStream fileOutputStream;
+  private FileChannel fileChannel;
+  private ByteBuffer buffer;
+  private int tuplesWrittenOnPage;
+  private int index;
+
+  public TupleWriter(String fileName) throws IOException {
+    File file = new File(fileName);
+    fileOutputStream = new FileOutputStream(file);
+    fileChannel = fileOutputStream.getChannel();
+    this.buffer = ByteBuffer.allocate(PAGE_SIZE); // Assuming each page has at most 4096 bytes
+    this.tuplesWrittenOnPage = 0;
+    this.index = 0;
+  }
+
+  public void writeTuple(Tuple tuple) throws IOException {
+    int tupleSize = tuple.getAllElements().size();
+
+    if (tuplesWrittenOnPage == 0) {
+      newPage(tupleSize);
+    } else if ((index + (tupleSize * 4)) > PAGE_SIZE) {
+
+      writePage();
+      newPage(tupleSize);
+
+    }
+
+    for (int i = 0; i < tupleSize; i++) {
+      buffer.putInt(tuple.getElementAtIndex(i));
+      index += 4;
+    }
+
+    this.tuplesWrittenOnPage++;
+    buffer.putInt(4, this.tuplesWrittenOnPage);
+  }
+
+  private void newPage(int tupleSize) {
+    buffer.clear();
+    buffer.put(new byte[PAGE_SIZE]);
+    buffer.clear();
+
+    buffer.putInt(tupleSize);
+    buffer.putInt(0);
+
+    tuplesWrittenOnPage = 0;
+    index = 8;
+  }
+
+  private void writePage() throws IOException {
+    while (buffer.hasRemaining()) {
+      buffer.putInt(0);
+    }
+
+    buffer.flip();
+    fileChannel.write(buffer);
+    buffer.clear();
+  }
+
+  public void close() throws IOException {
+    writePage();
+
+    fileChannel.close();
+    fileOutputStream.close();
+  }
+
+}
