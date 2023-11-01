333bf137205619ef13068d9ae8c706103e5bb9a0
jeffalipson <135264871+jeffalipson@users.noreply.github.com>
2023-11-01 14:09:59 -0400

    Original IndexScanOperator push

--- src/main/java/common/TreeIndex.java
+++ src/main/java/common/TreeIndex.java
@@ -11,7 +11,9 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class TreeIndex {
 
@@ -22,6 +24,7 @@ public class TreeIndex {
     Tuple nextTuple;
     ArrayList<Integer> nextRecord;
     static final int PAGE_SIZE = 4096;
+    int curPage;
 
     public TreeIndex(String fileName, Operator op, int order, int indexElement) {
         this.file = new File(fileName);
@@ -38,7 +41,7 @@ public class TreeIndex {
 
         ArrayList<int[]> leaves = new ArrayList<>();
 
-        while(nextTuple != null) {
+        while (nextTuple != null) {
             leaves.add(leafNode(op, indexElement));
         }
 
@@ -49,17 +52,16 @@ public class TreeIndex {
 
         nodes.set(0, headerNode(nodes.size() - 1, leaves.size(), order));
 
-        for(int i = 0; i < nodes.size(); i++) {
+        for (int i = 0; i < nodes.size(); i++) {
             try {
                 writeNode(nodes.get(i));
             } catch (IOException e) {
                 System.out.println("Tree loading failed");
             }
         }
-
     }
 
-    public TreeIndex (String filename) {
+    public TreeIndex(String filename) {
         this.file = new File(filename);
 
         try {
@@ -73,9 +75,99 @@ public class TreeIndex {
     }
 
     public int[] readNode(int page) {
-        int[] result = new int[PAGE_SIZE/4];
+        int[] result = new int[PAGE_SIZE / 4];
         buffer.clear();
-        buffer.asIntBuffer().get(result, (page + 1)*PAGE_SIZE/4, PAGE_SIZE/4);
+        buffer.asIntBuffer().get(result, (page + 1) * PAGE_SIZE / 4, PAGE_SIZE / 4);
+
+        curPage = page;
+        return result;
+    }
+
+    public int[] deserialize(int page, Integer traversekey) {
+        readNode(page);
+        if (buffer.getInt(0) == 1) {
+            return deserializeIndex(page, traversekey);
+        } else {
+            return deserializeLeaf(page);
+        }
+    }
+
+    // PROBABLY NOT RIGHT
+    public int[] getNextLeaf() {
+        return readNode(curPage + 1);
+    }
+
+    public int[] deserializeIndex(int page, Integer traversekey) {
+        int numKeys = buffer.getInt(4);
+
+        int[] keys = new int[numKeys];
+        int[] children = new int[numKeys + 1];
+
+        int pos = 8;
+        for (int i = 0; i < numKeys; i++) {
+            keys[i] = buffer.getInt(pos);
+            pos += 4;
+        }
+
+        for (int i = 0; i < numKeys + 1; i++) {
+            children[i] = buffer.getInt(pos);
+            pos += 4;
+        }
+
+        int index = 0;
+        while (index < numKeys && keys[index] < traversekey) {
+            index++;
+        }
+
+        return deserialize(children[index], traversekey);
+    }
+
+    private int[] deserializeLeaf(int page) {
+        List<Integer> keys = new ArrayList<>();
+        List<List<Integer>> entries = new ArrayList<>();
+
+        int pos = 4;
+        int numDataEntries = buffer.getInt(pos);
+        pos += 4;
+
+        for (int i = 0; i < numDataEntries; i++) {
+            int key = buffer.getInt(pos);
+            pos += 4;
+            keys.add(key);
+
+            int numRids = buffer.getInt(pos);
+            pos += 4;
+
+            List<Integer> rids = new ArrayList<>();
+            for (int j = 0; j < numRids; j++) {
+                int pageNum = buffer.getInt(pos);
+                pos += 4;
+                int tupleNum = buffer.getInt(pos);
+                pos += 4;
+
+                rids.add(pageNum);
+                rids.add(tupleNum);
+            }
+
+            entries.add(rids);
+        }
+
+        int[] result = new int[pos + numDataEntries * 2];
+        result[0] = page;
+        result[1] = numDataEntries;
+        result[2] = 0;
+
+        int index = 3;
+        for (Integer key : keys) {
+            result[index++] = key;
+        }
+
+        for (List<Integer> entry : entries) {
+            for (Integer rid : entry) {
+                result[index++] = rid;
+            }
+        }
+
         return result;
     }
 
@@ -83,35 +175,39 @@ public class TreeIndex {
         ArrayList<int[]> result = new ArrayList<>();
         int childrenStart = nodes.size() - children.size();
 
-        for(int i = order; i < children.size(); i += order) {
+        for (int i = order; i < children.size(); i += order) {
             int[] n = indexNode(children, childrenStart, order);
             nodes.add(n);
             result.add(n);
         }
 
-        if (result.size() == 1) return result;
-        else return indexNodeHelper(result, nodes, order);
+        if (result.size() == 1)
+            return result;
+        else
+            return indexNodeHelper(result, nodes, order);
     }
 
-    /*private void indexNodeHelper(ArrayList<int[]> nodes, int start, int order) {
-        int nodesAdded = 0;
-
-        for(int i = order; i < children.size(); i += order) {
-            int[] n = indexNode(children, start, order);
-            nodes.add(n);
-            nodesAdded++;
-        }
-
-        if (nodesAdded == 1) return;
-        else indexNodeHelper(nodes, start + resultSize, order);
-    }*/
+    /*
+     * private void indexNodeHelper(ArrayList<int[]> nodes, int start, int order) {
+     * int nodesAdded = 0;
+     * 
+     * for(int i = order; i < children.size(); i += order) {
+     * int[] n = indexNode(children, start, order);
+     * nodes.add(n);
+     * nodesAdded++;
+     * }
+     * 
+     * if (nodesAdded == 1) return;
+     * else indexNodeHelper(nodes, start + resultSize, order);
+     * }
+     */
 
     private int[] indexNode(ArrayList<int[]> nodes, int pointer, int order) {
-        int[] node = new int[PAGE_SIZE/4];
+        int[] node = new int[PAGE_SIZE / 4];
         node[0] = 1;
         node[1] = 0;
 
-        for(int i = 0; i < order; i++) {
+        for (int i = 0; i < order; i++) {
             node[i + 2] = nodes.get(i + pointer)[3];
             node[i + order + 2] = i + pointer;
         }
@@ -127,9 +223,10 @@ public class TreeIndex {
         result.add(nextTuple.getTID());
 
         nextTuple = op.getNextTuple();
-        if(nextTuple == null) return null;
+        if (nextTuple == null)
+            return null;
 
-        while(nextTuple != null && nextTuple.getElementAtIndex(index) == result.get(0)) {
+        while (nextTuple != null && nextTuple.getElementAtIndex(index) == result.get(0)) {
             result.add(nextTuple.getPID());
             result.add(nextTuple.getTID());
             result.set(1, result.get(1) + 1);
@@ -139,14 +236,14 @@ public class TreeIndex {
         return result;
     }
 
-    private int[] leafNode(Operator op, int index) {
-        int[] node = new int[PAGE_SIZE/4];
+    public int[] leafNode(Operator op, int index) {
+        int[] node = new int[PAGE_SIZE / 4];
         node[0] = 0;
         node[1] = 0;
         int i = 2;
 
         Iterator<Integer> record = nextRecord.iterator();
-        while(nextRecord != null) {
+        while (nextRecord != null) {
             while (record.hasNext()) {
                 try {
                     node[i] = record.next();
@@ -169,13 +266,11 @@ public class TreeIndex {
     }
 
     private int[] headerNode(int root, int leaves, int order) {
-        int[] node = new int[PAGE_SIZE/4];
-        node[0]= root;
+        int[] node = new int[PAGE_SIZE / 4];
+        node[0] = root;
         node[1] = leaves;
         node[2] = order;
         return node;
     }
-
-
 
 }
---
+++ src/main/java/physical_operator/IndexScanOperator.java
@@ -0,0 +1,106 @@
+package physical_operator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import net.sf.jsqlparser.expression.Alias;
+import common.Tuple;
+import common.TreeIndex;
+import common.DBCatalog;
+
+public class IndexScanOperator extends Operator {
+  public Integer lowkey;
+  public Integer highkey;
+  public TreeIndex tree;
+  public boolean clustered;
+  public int tuplePosition;
+  public ScanOperator scanner;
+  public int[] currentLeaf;
+  public int[] currentDataEntries;
+  public int dataEntryIndex;
+
+  public IndexScanOperator(String tableName, Alias alias, String indexColumnName, Integer lowkey, Integer highkey) {
+    super(null);
+    this.lowkey = lowkey;
+    this.highkey = highkey;
+    this.scanner = new ScanOperator(tableName, alias);
+
+    DBCatalog catalog = DBCatalog.getInstance();
+    ArrayList<String> indexInfo = catalog.getIndexInfo(tableName);
+    this.clustered = (indexInfo.get(1) == "1");
+
+    this.tree = new TreeIndex(catalog.getIndexDirectory() + '/' + tableName + '.' + indexColumnName);
+    this.tuplePosition = 0;
+
+    if (lowkey == null) {
+      currentLeaf = tree.readNode(1);
+    } else {
+      currentLeaf = tree.deserialize(1, lowkey);
+
+    }
+  }
+
+  public Tuple getNextTuple() {
+    if (clustered) {
+      Tuple tuple = scanner.getNextTuple();
+      if (tuple != null && tuplePosition >= lowkey && tuplePosition <= highkey) {
+        tuplePosition++;
+        return tuple;
+      }
+    } else {
+      if (currentLeaf != null) {
+        while (dataEntryIndex < currentDataEntries.length) {
+          int key = currentDataEntries[dataEntryIndex];
+          int recordID = currentDataEntries[dataEntryIndex + 1];
+
+          if (key >= lowkey && key <= highkey) {
+            Tuple tuple = getTupleFromRecordID(tree, recordID);
+
+            tuplePosition++;
+            dataEntryIndex += 2;
+            return tuple;
+          } else if (key > highkey) {
+            break;
+          } else {
+            dataEntryIndex += 2;
+          }
+        }
+
+        currentLeaf = tree.getNextLeaf();
+
+        if (currentLeaf != null) {
+          currentDataEntries = extractDataEntries(currentLeaf);
+          dataEntryIndex = 0;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  private Tuple getTupleFromRecordID(TreeIndex tree2, int recordID) {
+    return null;
+  }
+
+  @Override
+  public void reset() {
+    super.reset(0);
+  }
+
+  public int[] extractDataEntries(int[] leafNode) {
+    if (leafNode != null && leafNode[0] == 0) {
+      int numDataEntries = leafNode[1];
+      int[] dataEntries = new int[numDataEntries * 2];
+
+      for (int i = 0; i < numDataEntries; i++) {
+        int dataIndex = 2 + (i * 2);
+        dataEntries[i * 2] = leafNode[dataIndex];
+        dataEntries[i * 2 + 1] = leafNode[dataIndex + 1];
+      }
+      return dataEntries;
+    }
+    return null;
+  }
+
+}
