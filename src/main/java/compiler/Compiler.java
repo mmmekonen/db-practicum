5fcfac2a740e100da1dbde10f05a7ff7ec918283
mmmekonen <maelat.mekonen@gmail.com>
2023-11-14 16:46:38 -0500

    added logging message for stats

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -69,6 +69,7 @@ public class Compiler {
 
       // gather statistics
       gatherStats();
+      logger.info("Created database statistics");
 
       // Set up indexes
       logger.info("Building indexes...");
