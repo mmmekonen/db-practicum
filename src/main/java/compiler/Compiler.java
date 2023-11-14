e2468b798ef91aa26d0c8be61c6714dd9ae9701a
mmmekonen <maelat.mekonen@gmail.com>
2023-11-14 13:54:29 -0500

    updated interpreter config

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -15,7 +15,8 @@ import org.apache.logging.log4j.*;
 import physical_operator.Operator;
 
 /**
- * Top level harness class; reads queries from an input file one at a time, processes them and sends
+ * Top level harness class; reads queries from an input file one at a time,
+ * processes them and sends
  * output to file or to System depending on flag.
  */
 public class Compiler {
@@ -27,9 +28,6 @@ public class Compiler {
   private static String tempDir;
   private static final boolean outputToFiles = true; // true = output to
 
-  private static int buildIndexes;
-  private static int evalQueries;
-
   private static int joinType;
   private static int joinBuffer;
   private static int sortType;
@@ -39,10 +37,13 @@ public class Compiler {
   // to System.out
 
   /**
-   * Reads statements from queriesFile one at a time, builds query plan and evaluates, dumping
+   * Reads statements from queriesFile one at a time, builds query plan and
+   * evaluates, dumping
    * results to files or console as desired.
    *
-   * <p>If dumping to files result of ith query is in file named queryi, indexed stating at 1.
+   * <p>
+   * If dumping to files result of ith query is in file named queryi, indexed
+   * stating at 1.
    */
   public static void main(String[] args) {
 
@@ -56,68 +57,63 @@ public class Compiler {
     db.setIndexDirectory(inputDir + "/db/indexes");
     db.setIndexInfo();
 
-    // Set up indexes
     try {
       if (outputToFiles) {
-        for (File file : (new File(outputDir).listFiles())) file.delete(); // clean output directory
+        for (File file : (new File(outputDir).listFiles()))
+          file.delete(); // clean output directory
       }
 
-      if (buildIndexes == 1) {
-        logger.info("Building indexes...");
-        ArrayList<String> tables = new ArrayList<>();
-        tables.addAll(db.getIndexInfo().keySet());
+      // Set up indexes
+      logger.info("Building indexes...");
+      ArrayList<String> tables = new ArrayList<>();
+      tables.addAll(db.getIndexInfo().keySet());
 
-        for (int i = 0; i < tables.size(); i++) {
+      for (int i = 0; i < tables.size(); i++) {
 
-          ArrayList<String> info = db.getIndexInfo().get(tables.get(i));
+        ArrayList<String> info = db.getIndexInfo().get(tables.get(i));
 
-          TreeIndex t =
-              new TreeIndex(
-                  inputDir, tables.get(i), db.findColumnIndex(tables.get(i), info.get(0)), info);
-        }
-
-        logger.info("Indexes have been built");
+        TreeIndex t = new TreeIndex(
+            inputDir, tables.get(i), db.findColumnIndex(tables.get(i), info.get(0)), info);
       }
 
-      // check if building queries
-      if (evalQueries == 1) {
-        String str = Files.readString(Path.of(inputDir + "/queries.sql"));
-        Statements statements = CCJSqlParserUtil.parseStatements(str);
-        QueryPlanBuilder queryPlanBuilder = new QueryPlanBuilder();
+      logger.info("Indexes have been built");
 
-        int counter = 1; // for numbering output files
-        for (Statement statement : statements.getStatements()) {
-          // clean temp directory before each query
-          for (File file : (new File(tempDir).listFiles())) {
-            if (file.isDirectory()) {
-              for (File f : file.listFiles()) {
-                f.delete();
-              }
+      // run queries
+      String str = Files.readString(Path.of(inputDir + "/queries.sql"));
+      Statements statements = CCJSqlParserUtil.parseStatements(str);
+      QueryPlanBuilder queryPlanBuilder = new QueryPlanBuilder();
+
+      int counter = 1; // for numbering output files
+      for (Statement statement : statements.getStatements()) {
+        // clean temp directory before each query
+        for (File file : (new File(tempDir).listFiles())) {
+          if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+              f.delete();
             }
-            file.delete();
           }
-
-          logger.info("Processing query: " + statement);
-
-          try {
-            Operator plan =
-                queryPlanBuilder.buildPlan(statement, joinType, joinBuffer, sortType, sortBuffer);
-
-            if (outputToFiles) {
-              File outfile = new File(outputDir + "/query" + counter);
-              long timeElapsed = System.currentTimeMillis();
-              plan.dump(outfile);
-              timeElapsed = System.currentTimeMillis() - timeElapsed;
-              logger.info("Query processing time: " + timeElapsed + "ms");
-            } // else {
-            // plan.dump(System.out);
-            // }
-          } catch (Exception e) {
-            logger.error(e.getMessage());
-          }
-
-          ++counter;
+          file.delete();
         }
+
+        logger.info("Processing query: " + statement);
+
+        try {
+          Operator plan = queryPlanBuilder.buildPlan(statement, joinType, joinBuffer, sortType, sortBuffer);
+
+          if (outputToFiles) {
+            File outfile = new File(outputDir + "/query" + counter);
+            long timeElapsed = System.currentTimeMillis();
+            plan.dump(outfile);
+            timeElapsed = System.currentTimeMillis() - timeElapsed;
+            logger.info("Query processing time: " + timeElapsed + "ms");
+          } // else {
+          // plan.dump(System.out);
+          // }
+        } catch (Exception e) {
+          logger.error(e.getMessage());
+        }
+
+        ++counter;
       }
     } catch (Exception e) {
       System.err.println("Exception occurred in interpreter");
@@ -126,7 +122,8 @@ public class Compiler {
   }
 
   /**
-   * Reads the config file passed through the command line and sets all the parameters for how
+   * Reads the config file passed through the command line and sets all the
+   * parameters for how
    * queries should be built.
    */
   private static void readConfigFile() {
@@ -135,8 +132,6 @@ public class Compiler {
       inputDir = s.nextLine();
       outputDir = s.nextLine();
       tempDir = s.nextLine();
-      buildIndexes = s.nextInt();
-      evalQueries = s.nextInt();
     } catch (Exception e) {
       System.out.println(e + ": Could not find config file");
     }
@@ -147,9 +142,11 @@ public class Compiler {
     try {
       Scanner s = new Scanner(new File(inputDir + "/plan_builder_config.txt"));
       joinType = s.nextInt();
-      if (joinType == 1) joinBuffer = s.nextInt();
+      if (joinType == 1)
+        joinBuffer = s.nextInt();
       sortType = s.nextInt();
-      if (sortType == 1) sortBuffer = s.nextInt();
+      if (sortType == 1)
+        sortBuffer = s.nextInt();
       DBCatalog.getInstance().setUseIndex(s.nextInt() == 1 ? true : false);
     } catch (Exception e) {
       System.out.println(e + ": Could not find config file");
