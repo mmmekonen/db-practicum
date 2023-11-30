f487862fa0a05d4fa92506d493858133e5132576
mmmekonen <maelat.mekonen@gmail.com>
2023-11-30 14:49:19 -0500

    switched remainder from list to set

--- src/main/java/common/UFElement.java
+++ src/main/java/common/UFElement.java
@@ -15,25 +15,29 @@ public class UFElement {
   }
 
   ArrayList<String> attributes;
-  ArrayList<Expression> remainder;
+  HashSet<Expression> remainder;
   Long upperBound;
   Long lowerBound;
 
   public UFElement() {
     this.attributes = new ArrayList<>();
-    this.remainder = new ArrayList<>();
+    this.remainder = new HashSet<>();
   }
 
   public Long getEqualityCon() {
-    if (upperBound.equals(lowerBound)) return upperBound;
-    else return null;
+    if (upperBound.equals(lowerBound))
+      return upperBound;
+    else
+      return null;
   }
 
   public Expression getRemainder() {
     Expression e = null;
-    if (!remainder.isEmpty()) {
-      e = remainder.get(0);
-      for (int i = 1; i < remainder.size(); i++) e = new AndExpression(e, remainder.get(i));
+    ArrayList<Expression> remainderList = new ArrayList<>(remainder);
+    if (!remainderList.isEmpty()) {
+      e = remainderList.get(0);
+      for (int i = 1; i < remainderList.size(); i++)
+        e = new AndExpression(e, remainderList.get(i));
     }
     return e;
   }
@@ -60,18 +64,16 @@ public class UFElement {
 
   public void union(UFElement other) {
     this.attributes.addAll(other.attributes);
-    this.upperBound =
-        this.upperBound != null
-            ? (other.upperBound != null
-                ? Math.min(other.upperBound, this.upperBound)
-                : this.upperBound)
-            : (other.upperBound != null ? other.upperBound : Long.MAX_VALUE);
-    this.lowerBound =
-        this.lowerBound != null
-            ? (other.lowerBound != null
-                ? Math.max(other.lowerBound, this.lowerBound)
-                : this.lowerBound)
-            : (other.lowerBound != null ? other.lowerBound : Long.MIN_VALUE);
+    this.upperBound = this.upperBound != null
+        ? (other.upperBound != null
+            ? Math.min(other.upperBound, this.upperBound)
+            : this.upperBound)
+        : (other.upperBound != null ? other.upperBound : Long.MAX_VALUE);
+    this.lowerBound = this.lowerBound != null
+        ? (other.lowerBound != null
+            ? Math.max(other.lowerBound, this.lowerBound)
+            : this.lowerBound)
+        : (other.lowerBound != null ? other.lowerBound : Long.MIN_VALUE);
     this.remainder.addAll(other.remainder);
   }
 
